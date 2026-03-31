80d61cf9c677b986b864b9ead254b26a7939f9fb
Prachi-Gupta2808 <prachig2808@gmail.com>
2026-03-31 15:13:43 +0530

    added auth routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 const app = express();
@@ -9,7 +10,9 @@ app.use(cors());
 app.use(express.json());
 connectDB();
 
-//routes here later
+//routes here now
+app.use("/api/auth", authRoutes);
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
