0ba674f8e2ed0cb98f418b92b9772b213099a359
Prachi-Gupta2808 <prachig2808@gmail.com>
2026-04-06 15:13:09 +0530

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,12 @@ const authRoutes = require("./routes/authRoutes");
 const complaintRoutes = require("./routes/complaintRoutes");
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://civic-lens-snowy.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 connectDB();
 
